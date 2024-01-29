c062c3de02897fc396c9269948c8e909486fd1d6
Benjin Yap <benjyap7+github@gmail.com>
2024-01-29 16:52:05 -0500

    * Added summary clipboard functionality.

--- source/web/src/Pages/Home/Components/Receiptmajigger/ExpenseSummary.tsx
+++ source/web/src/Pages/Home/Components/Receiptmajigger/ExpenseSummary.tsx
@@ -1,6 +1,7 @@
-import {Checkbox, Divider, NumberInput, Stack, Table} from "@mantine/core";
+import {Button, Checkbox, Divider, NumberInput, Paper, Stack, Table} from "@mantine/core";
 import {useState} from "react";
 import classes from './ExpenseSummary.module.css';
+import {useClipboard} from "@mantine/hooks";
 
 export type ExpenseSummaryDataRow = {
   categoryId: number,
@@ -13,6 +14,8 @@ type ExpenseSummaryProps = {
 };
 
 const ExpenseSummary = (props: ExpenseSummaryProps) => {
+  const clipboard = useClipboard();
+
   const [includeTax, setIncludeText] = useState<boolean>(true);
   const [taxAmount, setTaxAmount] = useState<number>(13);
 
@@ -30,6 +33,18 @@ const ExpenseSummary = (props: ExpenseSummaryProps) => {
     }
 
     return copy;
+  };
+
+  const finalData: Array<ExpenseSummaryDataRow> = calculateFinalAmounts();
+
+  const generateCopyText = (): string => {
+    let str = '';
+
+    for (const row of finalData) {
+      str += `${row.categoryName},${row.amount}\n`;
+    }
+
+    return str;
   };
 
   return (
@@ -52,27 +67,36 @@ const ExpenseSummary = (props: ExpenseSummaryProps) => {
         suffix="%"
         disabled={!includeTax}
       />
-      <Table>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>Category</Table.Th>
-            <Table.Th>Total</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {calculateFinalAmounts().map((a: ExpenseSummaryDataRow) => {
-            return (
-              <Table.Tr
-                key={a.categoryId}
-                className={classes[`row${a.categoryId}`]}
-              >
-                <Table.Td>{a.categoryName}</Table.Td>
-                <Table.Td>{`$${a.amount}`}</Table.Td>
-              </Table.Tr>
-            );
-          })}
-        </Table.Tbody>
-      </Table>
+      <Paper
+        withBorder
+      >
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>Category</Table.Th>
+              <Table.Th>Total</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {finalData.map((a: ExpenseSummaryDataRow) => {
+              return (
+                <Table.Tr
+                  key={a.categoryId}
+                  className={classes[`row${a.categoryId}`]}
+                >
+                  <Table.Td>{a.categoryName}</Table.Td>
+                  <Table.Td>{`$${a.amount}`}</Table.Td>
+                </Table.Tr>
+              );
+            })}
+          </Table.Tbody>
+        </Table>
+      </Paper>
+      <Button
+        onClick={() => clipboard.copy(generateCopyText())}
+      >
+        {clipboard.copied ? 'Copied' : 'Copy as CSV Text'}
+      </Button>
     </Stack>
   );
 }
